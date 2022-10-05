1d2744bfa46edde8a295b013a5eaff5b74830373
Polina Somova <polina.smva@gmail.com>
2022-10-05 22:39:56 +0300

    Initial commit

---
+++ TApplication.h
@@ -0,0 +1,15 @@
+#ifndef LAB1_TAPPLICATION_H
+#define LAB1_TAPPLICATION_H
+
+
+
+class TApplication {
+
+public:
+    TApplication(int, char **);
+    int run();
+
+};
+
+
+#endif //LAB1_TAPPLICATION_H
---
+++ number.h
@@ -0,0 +1,6 @@
+#ifndef LAB1_NUMBER_H
+#define LAB1_NUMBER_H
+
+typedef double number;
+
+#endif //LAB1_NUMBER_H
